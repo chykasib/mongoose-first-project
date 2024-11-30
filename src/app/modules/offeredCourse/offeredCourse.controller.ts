6b197cc141f6c5820fd0a7bed2649a2f1be4f350
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2024-11-30 23:55:23 +0600

    implemented offer course create funtionality

---
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { StatusCodes } from 'http-status-codes';
+import { OfferedCourseServices } from './offeredCourse.service';
+const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
+  const result = await OfferedCourseServices.createOfferedCourseIntoDB(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Offered Course is created successfully !',
+    data: result,
+  });
+});
+
+const getAllOfferedCourses = catchAsync(async (req: Request, res: Response) => {
+  const result = await OfferedCourseServices.getAllOfferedCoursesFromDB(
+    req.query,
+  );
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'OfferedCourses retrieved successfully !',
+    data: result,
+  });
+});
+
+export const OfferedCourseControllers = {
+  createOfferedCourse,
+  getAllOfferedCourses,
+};
